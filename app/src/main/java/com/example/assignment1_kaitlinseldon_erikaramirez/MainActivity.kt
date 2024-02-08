fcfe05fec4567aaf4e48588303ea38c8dd7f8a9f
seldo002 <seldo002@csusm.edu>
2024-02-08 14:27:23 -0800

    commit - 3 _ 2/8/24 - 2:26pm

--- app/src/main/java/com/example/assignment1_kaitlinseldon_erikaramirez/MainActivity.kt
+++ app/src/main/java/com/example/assignment1_kaitlinseldon_erikaramirez/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.assignment1_kaitlinseldon_erikaramirez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        val spinner = findViewById<Spinner>(R.id.name_department_year)
+        val img = findViewById<ImageView>(R.id.profile_picture)
+
+        val spinnerItems = resources.getStringArray(R.array.profiles)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerItems)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                when (spinnerItems[position]) {
+                    "Kaitlin Seldon, Computer Science, 2024" -> img.setImageResource(R.drawable.avatar_4_raster)
+                    "Erika Ramirez, Computer Science, 2024" -> img.setImageResource(R.drawable.avatar_8_raster)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+}}
